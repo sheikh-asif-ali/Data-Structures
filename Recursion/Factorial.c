b91b6891df3c208616d08ce454855fafb9ca1741
Asif Ali <asifali@blr1.cdac.in>
2025-03-14 17:17:33 +0530

    Recursion problem

--- Recursion/Factorial.c
+++ Recursion/Factorial.c
@@ -1,18 +1,28 @@
 // To calculate factorial of a number using recursion
 #include <stdio.h>
-int fact(int n)
+int fact(int n) //recursive function
 {
     if (n==0)
         return 1;
     return fact(n-1)*n;
 }
-
+int Ifact(int n) //itterative function
+{
+    int f=1;
+    for(int i=1;i<=n;i++)
+        f=f*i;
+    return f;
+}
 int main()
 {
-    int output=0;
     int input=3;
-    output = fact(input);
-    printf("Factorial of %d is %d by recursion", input, output);
+    int output1 = fact(input);
+    printf("Factorial of %d is %d by recursion", input, output1);
+    return 0;
+
+    int output2 = Ifact(input);
+    printf("Factorial of %d is %d by itteration", input, output2);
     return 0;
 }
 // Output: Factorial of 3 is 6 by recursion
+//         Factorial of 3 is 6 by itteration
