cdb4d6429539237687e0c71352b1cb3ce221fcae
sheikh-asif-ali <asifali9329@gmail.com>
2026-05-29 23:27:37 +0530

    string problem

---
+++ String/withoutloop.c
@@ -0,0 +1,16 @@
+// Print without using loop
+#include <stdio.h>
+int num=1;
+void printname(char ch[]){
+    printf("%d %s \n",num, ch);
+    num++;
+    if(num<=100)
+    {
+         printname(ch);
+    };
+}
+int main() {
+    char ch[] = "Asif";
+    printname(ch);
+    return 0;
+}
